47a46202deff85a60b37e6a9e3e12b3b26776b5d
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-12-15 02:20:19 +0900

    <변경>: <불필요한 오브젝트 제거함>

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 class Habit extends Component {
-  state = {
-    count: 0,
-  };
   handleIncrement = (event) => {
     this.props.onIncrement(this.props.habit);
   };
@@ -19,22 +16,13 @@ class Habit extends Component {
       <>
         <span className="habit-name">{name}</span>
         <span className="habit-count">{count}</span>
-        <button
-          className="habit-button habit-increase"
-          onClick={this.handleIncrement}
-        >
+        <button className="habit-button habit-increase" onClick={this.handleIncrement}>
           <i className="fas fa-plus-square"></i>
         </button>
-        <button
-          className="habit-button habit-decrease"
-          onClick={this.handleDecrement}
-        >
+        <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
           <i className="fas fa-minus-square"></i>
         </button>
-        <button
-          className="habit-button habit-delete"
-          onClick={this.handleDelete}
-        >
+        <button className="habit-button habit-delete" onClick={this.handleDelete}>
           <i className="fas fa-trash"></i>
         </button>
       </>
