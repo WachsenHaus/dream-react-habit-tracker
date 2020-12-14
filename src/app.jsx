dac8ae6dca4e898c38667bf8098cc603aa87e45a
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-12-15 03:21:14 +0900

    <추가>: <navbar를 추가하였음>
    * 최상위 app에서 로직들을 정의하였음 규모가 큰 프로젝트를 만들때는 컴포넌트 별로
    * 각각의 state를 가지고있다면 그 컴포넌트 안에서 로직을 처리하는것이 맞음.

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,58 @@
-import React from 'react';  
-import './app.css';
-import Habit from './components/habit';
-import Habits from './components/habits';
+import React, { Component } from "react";
+import "./app.css";
+import HabitAddForm from "./components/habitAddForm";
+import Habits from "./components/habits";
+import Navbar from "./components/navbar";
 
+class App extends Component {
+  state = {
+    totalCount: 3,
+    habits: [
+      { id: 1, name: "Reading", count: 0 },
+      { id: 2, name: "Running", count: 0 },
+      { id: 3, name: "Coding", count: 0 },
+    ],
+  };
+  changeCount = (length) => {
+    const totalCount = length;
+    this.setState({ totalCount });
+  };
+  handleIncrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count++;
+    this.setState({ habits });
+    this.changeCount(habits.length);
+  };
+  handleDecrement = (habit) => {
+    const habits = [...this.state.habits];
+    const index = habits.indexOf(habit);
+    habits[index].count < 1 ? (habits[index].count = 0) : habits[index].count--;
 
-function App() {
-  return <Habits/>;
+    this.setState({ habits });
+    this.changeCount(habits.length);
+  };
+  handleDelete = (habit) => {
+    const habits = this.state.habits.filter((item) => item.id !== habit.id);
+    this.setState({ habits });
+    this.changeCount(habits.length);
+  };
+  handleAdd = (habit) => {};
+
+  render() {
+    return (
+      <>
+        <Navbar totalCount={this.state.totalCount} />
+        <HabitAddForm onAdd={this.handleAdd} />
+        <Habits
+          habits={this.state.habits}
+          onIncrement={this.handleIncrement}
+          onDecrement={this.handleDecrement}
+          onDelete={this.handleDelete}
+        />
+      </>
+    );
+  }
 }
+
 export default App;
--- src/components/habits.jsx
+++ src/components/habits.jsx
@@ -3,41 +3,19 @@ import Habit from "./habit";
 
 ////
 class Habits extends Component {
-  state = {
-    habits: [
-      { id: 1, name: "Reading", count: 0 },
-      { id: 2, name: "Running", count: 0 },
-      { id: 3, name: "Coding", count: 0 },
-    ],
-  };
   handleIncrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count++;
-
-    this.setState({ habits }); // {habits:habits} 키와 밸류로서 로컬변수를 가르킨다. 동일할경우 생략이 가능하다.
+    this.props.onIncrement(habit);
   };
   handleDecrement = (habit) => {
-    const habits = [...this.state.habits];
-    const index = habits.indexOf(habit);
-    habits[index].count < 1 ? (habits[index].count = 0) : habits[index].count--;
-
-    this.setState({ habits });
+    this.props.onDecrement(habit);
   };
   handleDelete = (habit) => {
-    // const habits = [...this.state.habits];
-    // const index = habits.indexOf(habit);
-    // habits.splice(index, 1);
-    // this.setState({ habits });
-    //필터를 써보자... 구닥다리 방식은 이제그만
-    const habits = this.state.habits.filter((item) => item.id !== habit.id); //필터함수는 해당하는 조건에 맞는 요소들을 모아 새로운 배열을 반환한다.
-    //클릭한 아이디와 다른애들만 남게되는 함수이지
-    this.setState({ habits });
+    this.props.onDelete(habit);
   };
   render() {
     return (
       <ul>
-        {this.state.habits.map((habit) => (
+        {this.props.habits.map((habit) => (
           <Habit
             key={habit.id}
             habit={habit}
