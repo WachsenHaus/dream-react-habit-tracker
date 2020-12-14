0877ab6805013d8d3607d1e18ac707abe1f7fb58
Younghoon CHOI _^ <wachsenhaus@gmail.com>
2020-12-15 02:12:01 +0900

    <추가>: <이벤트 처리하기 추가함>
    *  {habits:habits} 키와 밸류로서 로컬변수를 가르킨다. 동일할경우 생략이 가능하다.
    * 필터함수는 해당하는 조건에 맞는 요소들을 모아 새로운 배열을 반환한다.
    * ...(spread)함수는 배열을 복사한다.
    * key는 react가 어떤 항목을 변경,추가 또는 삭제할지 식별하는것을 돕는다.

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,38 +1,45 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Habit extends Component {
-    state = {
-        count: 0,
-    };
-    handleIncrement = (event) =>{
-        //state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야 함
-        this.setState({count: this.state.count +1});
-        //this.state.count +=1; 이렇게 하면 리액트에서는 업데이트 되었는지 모른다. 그렇기 때문에 꼭 setstate를 사용해야한다.
-    }
-    handleDecrement = (event) =>{
-        //state 오브젝트 안에 있는 count를 증가 한 뒤 state를 업데이트 해야 함
-        const count = this.state.count - 1;
-        this.setState({count: count<0?0:count});
-        //this.state.count +=1; 이렇게 하면 리액트에서는 업데이트 되었는지 모른다. 그렇기 때문에 꼭 setstate를 사용해야한다.
-    }
-    render() {
-        const {name,count} = this.props.habit;
-       return (
-           <>
-            <span className="habit-name">{name}</span>
-            <span className="habit-count">{count}</span>
-            <button className="habit-button habit-increase" onClick={this.handleIncrement}>
-                <i className="fas fa-plus-square"></i>
-            </button>
-            <button className="habit-button habit-decrease" onClick={this.handleDecrement}>
-                <i className="fas fa-minus-square"></i>
-            </button>
-            <button className="habit-button habit-delete">
-                <i className="fas fa-trash"></i>
-            </button>
-           </>
-       );
-    }
+  state = {
+    count: 0,
+  };
+  handleIncrement = (event) => {
+    this.props.onIncrement(this.props.habit);
+  };
+  handleDecrement = (event) => {
+    this.props.onDecrement(this.props.habit);
+  };
+  handleDelete = (event) => {
+    this.props.onDelete(this.props.habit);
+  };
+  render() {
+    const { name, count } = this.props.habit;
+    return (
+      <>
+        <span className="habit-name">{name}</span>
+        <span className="habit-count">{count}</span>
+        <button
+          className="habit-button habit-increase"
+          onClick={this.handleIncrement}
+        >
+          <i className="fas fa-plus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-decrease"
+          onClick={this.handleDecrement}
+        >
+          <i className="fas fa-minus-square"></i>
+        </button>
+        <button
+          className="habit-button habit-delete"
+          onClick={this.handleDelete}
+        >
+          <i className="fas fa-trash"></i>
+        </button>
+      </>
+    );
+  }
 }
 
 export default Habit;
